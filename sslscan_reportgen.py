a3f596efa74fad658a8534cab7f156654c401398
bharshbarger <bharshbarger@users.noreply.github.com>
2017-11-15 22:09:08 -0600

    Update sslscan_reportgen.py

--- sslscan_reportgen.py
+++ sslscan_reportgen.py
@@ -8,7 +8,10 @@ try:
     import sys
     import time
     from docx.shared import Pt
-    from docx.shared import RGBColor
+    from docx.shared import Inches
+    from docx.shared import RGBColor    
+    from libnmap.parser import NmapParser
+
 except Exception as e:
     print('\n[!] Import(s) failed!: {}'.format(e))
 
@@ -62,6 +65,32 @@ class sslscan_beautifier():
         #clean up client name, remove spaces and stuff
         self.args.client = ''.join(e for e in self.args.client if e.isalnum())
 
+    def read_nmap(self):
+        nmap_report = NmapParser.parse_fromfile()
+        list443 = []
+        list1 = [ a.address for a in nmap_report.hosts if (a.get_open_ports()) and 443 in [b[0] for b in a.get_open_ports()] ]
+        for x in list1:
+            list443.append(x+":443")
+        list8443 = []
+        list2 = [ a.address for a in nmap_report.hosts if (a.get_open_ports()) and 8443 in [b[0] for b in a.get_open_ports()] ]
+        for x in list2:
+            list8443.append(x+":8443")
+        mergedlist = list443 + list8443
+        mergedlist.sort()
+        new1 = '\n'.join(mergedlist)
+        return new1
+
+
+    def run_sslscan(self):
+        '''multiprocess here'''
+        a = True
+
+
+    def run_nmap(self):
+        #nmap -sV --script ssl-enum-ciphers -p 443 <host>
+        a  = True
+
+
     def read_file(self):
         """open targets file, splits on ip and puts ip and the result into a dictionary"""
         #strip out ansi control chars (sometimes these appear in colorized output)
@@ -110,161 +139,171 @@ class sslscan_beautifier():
                         self.heartbleed_dict[key] = line
 
     def gen_report(self):
-        """create a document -- to use a template put the docx in the (), like (my_template.docx)"""
-        document = docx.Document()
+
         print ('\n**** Generating Report: ****')
         print('**** {}sslscan_{}.docx ****\n'.format(self.report_dir, self.args.client))
-        #optional header
+
+        """create a document -- to use a template put the docx in the (), like (my_template.docx)"""
+        document = docx.Document()
+
+
+        #header
         heading = document.add_heading(level=3)
         run_heading = heading.add_run('Hosts With Weak Transport Security')
         font = run_heading.font
         font.name = 'Arial'
         font.size = Pt(24)
         font.color.rgb = RGBColor(0xe9, 0x58, 0x23)
+
+        #create output table
+        rc4_table = document.add_table(rows=1, cols=2)
+        rc4_table.style = 'Table Grid'
+        if self.rc4_dict:
+            heading_cells = rc4_table.rows[0].cells
+            heading_cells[0].text = 'RC4 Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.rc4_dict.items():
+                cells = rc4_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
         paragraph = document.add_paragraph()
-        #start by printing the ip address in 20pt arial
-        #for each ip parsed out, look thru the results and add them to the report
-        for key, val in sorted(self.result_dictionary.items()):
-            run_paragraph = paragraph.add_run(key)
-            font = run_paragraph.font
-            font.name = 'Arial'
-            font.size = Pt(20)
-            run_paragraph = paragraph.add_run('\n')
-            if self.args.verbose is True:
-                print('Added host: {}'.format(key))
-            #add RC4 findings
-            if self.rc4_dict:
-                run_paragraph = paragraph.add_run('RC4 Support\n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.rc4_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.sslv2_dict:
-                #if this host had sslv2, print the header
-                run_paragraph = paragraph.add_run('SSLv2 Support\n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.sslv2_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.sslv3_dict:
-                #add sslv3 finding
-                run_paragraph = paragraph.add_run('SSLv3 Support\n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.sslv3_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.des_dict:
-                #add des finding
-                run_paragraph = paragraph.add_run('DES Support\n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.des_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.tls10_dict:
-                #add tls 1.0 findings
-                run_paragraph = paragraph.add_run('TLS v1.0\n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.tls10_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.weakbits_dict:
-                #add weak key findings
-                run_paragraph = paragraph.add_run('Weak key size \n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.weakbits_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.heartbleed_dict:
-                #add heartbleed findings
-                run_paragraph = paragraph.add_run('Heartbleed \n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                for i, r in self.heartbleed_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-            if self.md5_dict:
-                #add MD5 findings
-                run_paragraph = paragraph.add_run('MD5 Supported \n')
-                font = run_paragraph.font
-                font.name = 'Arial'
-                font.size = Pt(16)
-                #enumerate the lines were rc4 was found
-                for i, r in self.md5_dict.items():
-                    run_paragraph = paragraph.add_run(r)
-                    font = run_paragraph.font
-                    font.name = 'Arial'
-                    font.size = Pt(11)
-                    run_paragraph = paragraph.add_run('\n')
-        document.save('{}sslscan_{}.docx'.format(self.report_dir, self.args.client))
+        #run_paragraph = paragraph.add_run('\n')
+
+        sslv2_table =  document.add_table(rows=1, cols=2)
+        sslv2_table.style = 'Table Grid'
+        if self.sslv2_dict:
+            heading_cells = sslv2_table.rows[0].cells
+            heading_cells[0].text = 'SSLv2 Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.sslv2_dict.items():
+                cells = sslv2_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        paragraph = document.add_paragraph()
+        #run_paragraph = paragraph.add_run('\n')
+
+        sslv3_table =  document.add_table(rows=1, cols=2)
+        sslv3_table.style = 'Table Grid'
+        if self.sslv3_dict:
+            heading_cells = sslv3_table.rows[0].cells
+            heading_cells[0].text = 'SSLv3 Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.sslv3_dict.items():
+                cells = sslv3_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        paragraph = document.add_paragraph()
+        #run_paragraph = paragraph.add_run('\n')
+
+        des_table =  document.add_table(rows=1, cols=2)
+        des_table.style = 'Table Grid'
+        if self.des_dict:
+            heading_cells = des_table.rows[0].cells
+            heading_cells[0].text = 'DES Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.des_dict.items():
+                cells = des_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        paragraph = document.add_paragraph()
+        #run_paragraph = paragraph.add_run('\n')
+
+        tls10_table =  document.add_table(rows=1, cols=2)
+        tls10_table.style = 'Table Grid'
+        if self.tls10_dict:
+            heading_cells = tls10_table.rows[0].cells
+            heading_cells[0].text = 'TLS v1.0 Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.tls10_dict.items():
+                cells = tls10_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        paragraph = document.add_paragraph()
+        #run_paragraph = paragraph.add_run('\n')
+
+        weakbits_table =  document.add_table(rows=1, cols=2)
+        weakbits_table.style = 'Table Grid'
+        if self.weakbits_dict:
+            heading_cells = weakbits_table.rows[0].cells
+            heading_cells[0].text = 'Weak Key Length Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.weakbits_dict.items():
+                cells = weakbits_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        paragraph = document.add_paragraph()
+        #run_paragraph = paragraph.add_run('\n')
+
+
+        heartbleed_table =  document.add_table(rows=1, cols=2)
+        heartbleed_table.style = 'Table Grid'
+        if self.heartbleed_dict:
+            heading_cells = heartbleed_table.rows[0].cells
+            heading_cells[0].text = 'Heartbleed Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.heartbleed_dict.items():
+                cells = heartbleed_able.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        paragraph = document.add_paragraph()
+        #run_paragraph = paragraph.add_run('\n')
+
+
+        md5_table =  document.add_table(rows=1, cols=2)
+        md5_table.style = 'Table Grid'
+        if self.md5_dict:
+            heading_cells = md5_table.rows[0].cells
+            heading_cells[0].text = 'MD5 Hosts'
+            heading_cells[1].text = 'Supported Suites'
+            for i, r in self.md5_dict.items():
+                cells = md5_table.add_row().cells
+                cells[0].text = str(i)
+                cells[1].text = str(r)
+
+        document.save('{}sslscan_{}.docx'.format(self.report_dir, self.args.client.strip().encode()))
+
 
     def print_summary(self):
         """displays what was found to terminal"""
         if self.rc4_dict:
             print('\n********RC4 Hosts********')
             for k, v in sorted(self.rc4_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.sslv2_dict:
             print('\n********SSLv2 Hosts********')
             for k, v in sorted(self.sslv2_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.sslv3_dict:
             print('\n********SSLv3 Hosts********')
             for k, v in sorted(self.sslv3_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.des_dict:
             print('\n********DES Hosts********')
             for k, v in sorted(self.des_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.tls10_dict:
             print('\n********TLS v1.0 Hosts********')
             for k, v in sorted(self.tls10_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.weakbits_dict:
             print('\n********Weak Key Size********')
             for k, v in sorted(self.weakbits_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.heartbleed_dict:
             print('\n********Heartbleed Hosts********')
             for k, v in sorted(self.heartbleed_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
         if self.md5_dict:
             print('\n********MD5 Hosts********')
             for k, v in sorted(self.md5_dict.items()):
-                print('{}'.format(k))
+                print('{} {}'.format(k, v))
 
     def end(self):
         """ending stuff, right now just shows how long script took to run"""
@@ -276,6 +315,7 @@ def main():
     parser.add_argument('-c', '--client', metavar='<client>', help='client name')
     parser.add_argument('-f', '--file', metavar='<file>', help='file to read')
     parser.add_argument('-v', '--verbose', help='Optionally enable verbosity', action='store_true')
+    parser.add_argument('-x',  '--xml', help='nmap xml output to read')
     args = parser.parse_args()
     run = sslscan_beautifier(args)
     run.check_args(parser)
